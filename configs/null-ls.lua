bb9bf6d4155806c560be5e8d04c5a42c0626e939
Aly Mohamed Hassan <alyhassan10@hotmail.com>
2024-02-28 12:43:26 +0200

    feat: add null ls

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -2,15 +2,41 @@ local null_ls = require "null-ls"
 
 local formatting = null_ls.builtins.formatting
 local lint = null_ls.builtins.diagnostics
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local sources = {
-   formatting.prettier,
-   formatting.stylua,
+  formatting.prettierd,
+  formatting.black,
+  formatting.eslint_d.with {
+    filetypes = { "typescript", "javascript" },
+  },
+  formatting.stylua,
+  formatting.reorder_python_imports,
+  formatting.stylua,
+  formatting.gofumpt,
+  formatting.terraform_fmt.with {
+    filetypes = { "terraform", "tf", "terraform-vars", "hcl" },
+  },
+  formatting.sqlfmt,
 
-   lint.shellcheck,
+  lint.shellcheck,
+  lint.ruff,
+  lint.eslint_d,
 }
 
 null_ls.setup {
-   debug = true,
-   sources = sources,
+  debug = true,
+  sources = sources,
+  on_attach = function(client, bufnr)
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format { async = false }
+        end,
+      })
+    end
+  end,
 }
