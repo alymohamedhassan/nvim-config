89f6282b24414dd55f8e544616cd1d8c44f1d794
Aly Mohamed Hassan <19504642+alymohamedhassan@users.noreply.github.com>
2026-03-08 01:53:04 +0200

    add(salesforce): implement keymaps for OAuth process management and org login

--- lua/plugins/salesforce.lua
+++ lua/plugins/salesforce.lua
@@ -14,6 +14,12 @@ return {
       vim.keymap.set("n", "<leader>sfp", Sf.save_and_push, { desc = "(Salesforce) Save And Push To Target Org" })
       vim.keymap.set("n", "<leader>sfs", Sf.set_target_org, { desc = "(Salesforce) set local" })
       vim.keymap.set("n", "<leader>sfS", Sf.set_global_target_org, { desc = "(Salesforce) set global" })
+
+      -- Org auth: open terminal and run web login (browser will open)
+      vim.keymap.set("n", "<leader>sfl", function()
+        local cmd = vim.fn.executable("sf") == 1 and "sf org login web" or "sfdx force:auth:web:login"
+        vim.cmd("split | resize 8 | terminal " .. cmd)
+      end, { desc = "(Salesforce) Org login (web)" })
     end,
   },
 }
